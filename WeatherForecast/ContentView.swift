4d7e1e8e868ab5c0ab618fcceae50e6c19f846a3
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-01 00:14:19 +0100

    Fix initialiser errors

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -10,9 +10,9 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack {
-            DayForecast(day: "Mon")
+            DayForecast(day: "Mon", high: 70, low: 50)
             
-            DayForecast(day: "Tue")
+            DayForecast(day: "Tue", high: 60, low: 40)
         }
     }
 }
