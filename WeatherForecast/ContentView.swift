d4fc7e6cb98978222af2cec486cd5e6268e46941
ITHelpDec <34002836+ITHelpDec@users.noreply.github.com>
2024-05-11 17:47:07 +0100

    Part 1 - Views, structures, and properties (#2)

--- WeatherForecast/ContentView.swift
+++ WeatherForecast/ContentView.swift
@@ -9,16 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            DayForecast(day: "Mon", isRainy: false, high: 70, low: 50)
+            
+            DayForecast(day: "Tue", isRainy: true, high: 60, low: 40)
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DayForecast: View {
+    let day: String
+    let isRainy: Bool
+    let high: Int
+    let low: Int
+    
+    var iconName: String {
+        return isRainy ? "cloud.rain.fill" : "sun.max.fill"
+    }
+    
+    var iconColour: Color {
+        return isRainy ? Color.blue : Color.yellow
+    }
+    
+    var body: some View {
+        VStack {
+            Text(day)
+                .font(.headline)
+            Image(systemName: iconName)
+                .foregroundStyle(iconColour)
+                .font(.largeTitle)
+                .padding(5)
+            Text("High: \(high)ºF")
+                .fontWeight(.semibold)
+            Text("Low: \(low)ºF")
+                .fontWeight(.medium)
+                .foregroundStyle(.secondary)
+        }
+        .padding()
+    }
 }
